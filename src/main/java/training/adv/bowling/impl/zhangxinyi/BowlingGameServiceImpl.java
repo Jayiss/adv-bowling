21ac4bb63562affcdb771414acb80273cf816753
alo1719 <1335250440@qq.com>
2019-10-11 14:29:10 +0800

    commit before merge

--- src/main/java/training/adv/bowling/impl/zhangxinyi/BowlingGameServiceImpl.java
+++ src/main/java/training/adv/bowling/impl/zhangxinyi/BowlingGameServiceImpl.java
@@ -3,4 +3,10 @@ package training.adv.bowling.impl.zhangxinyi;
 import training.adv.bowling.impl.GameService;
 
 public class BowlingGameServiceImpl extends GameService {
+
+    @Override
+    public void play() {
+        System.out.println("Now Playing!");
+
+    }
 }
